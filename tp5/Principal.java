22ae3e815ae7973eaa9fe4ffb5127b8b0f98d67b
Lucas Ariel Fernandez <lucasfernandez999@gmail.com>
2025-11-20 15:50:55 -0300

    Practica con recursividad

--- tp5/Principal.java
+++ tp5/Principal.java
@@ -7,11 +7,27 @@ public class Principal {
 
         Scanner sc = new Scanner(System.in);
         
-        int num;
+        int num, num2;
 
+        //Factorial
         System.out.print("Factorial de: ");
         num=sc.nextInt();
         System.out.println("Es: "+Factorial.fact(num));
+
+        //Puzzle tp1
+        System.out.println("Puzzle de: ");
+        System.out.print("Base: ");
+        num=sc.nextInt();
+        System.out.print("Limite: ");
+        num2=sc.nextInt();
+
+        System.out.println("Resultado 1: "+puzzle.rompecabezas(num, num2));
+
+        System.out.println("2: "+puzzle.rompecabezas(14, 10));
+        System.out.println("3: "+puzzle.rompecabezas(4, 7));
+        System.out.println("4: "+puzzle.rompecabezas(0, 0));
+
+
         
         sc.close();
         
